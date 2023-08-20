08c8277301d60bfa8427b7a6247d2a61bce6d290
Quang Nguyen <n.trongquang2002@gmail.com>
2023-08-20 11:51:14 +0700

    delete redundant files

--- back-end/App.js
+++ back-end/App.js
@@ -1,24 +1,16 @@
 var express = require("express");
 var config = require("config");
 var bodyParse = require("body-parser");
-var engines = require('consolidate');
 var cors = require('cors');
-
-
 var app = express();
 
 app.use(cors());
-// dùng để đọc được data nhận
 app.use(bodyParse.json());
-app.use(bodyParse.urlencoded({extended: true}));
-
-app.use("/static", express.static(__dirname + "/public"));
-
+app.use(bodyParse.urlencoded({ extended: true }));
 app.use(require(__dirname + "/apps/controller"));
 
 var host = config.get("server.host");
 var port = config.get("server.port");
-
-app.listen(port, host, function(){
+app.listen(port, host, function () {
     console.log("Server is running on port", port);
 })
